227bfdc98c5a062d94f2a33052471c5fc29c1f8c
小野和久 <k.a-z.ono@betelgeuse-corp.com>
2018-12-23 18:35:52 +0900

    修正

--- src/game.js
+++ src/game.js
@@ -10,43 +10,10 @@ exports.handler = async function(event,context,callback){
     callback(null,{})
   }
 
+  const messageKey = matchResult[0]
   const data = {
     replyToken:targetEvent.replyToken,
-    messages:[
-      {
-      type:'text',
-      text:'技術書店の締め切りまであと7日, だけど進捗は10% あなたはどうする？',
-      quickReply:{
-        items:[
-          {
-            type:"actin",
-            action:{
-              type:'message',
-              label:'限界まで頑張る',
-              text:'[1]限界まで頑張る'
-            }
-          },
-          {
-            type:"actin",
-            action:{
-              type:'message',
-              label:'寝る',
-              text:'[2]寝る'
-            }
-          },
-          {
-            type:"actin",
-            action:{
-              type:'message',
-              label:'出店をやめる',
-              text:'[3]出店をやめる'
-            }
-          },
-
-          ]
-        }
-      }
-    ]
+    messages:[messageData[messageKey]]
   }
 
   const res = await axios.post('https://api.line.me/v2/bot/message/reply',data,{
