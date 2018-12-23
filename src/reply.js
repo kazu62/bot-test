d2ddfc97cc5a3d2dff5cdcc27003516b1b8cc8f8
小野和久 <k.a-z.ono@betelgeuse-corp.com>
2018-12-23 16:42:47 +0900

    修正

--- src/reply.js
+++ src/reply.js
@@ -1,27 +1,29 @@
 import axios from 'axios'
 
-exports.handler = async function(event,context,callback){
+exports.handler = async function(event, context, callback) {
   const webhookBody = JSON.parse(event.body)
   console.log(webhookBody)
 
   const data = {
-    replyToken:webhookBody.events[0].replyToken,
-    messages:[
+    replyToken: webhookBody.events[0].replyToken,
+    messages: [
       {
-        type:'text',
-        text:'HELLO Netlify Bot'
+        type: 'text',
+        text: 'Hello Netlify Bot'
       }
     ]
   }
-  const res = await axios.post('https://api.line.me/v2/bot/message/reply',data,{
-    headers:{
-      'Content-Type':'application/json',
-      'Authorization':`Bearer ${process.env.CHANNEL_TOKEN}`
+
+  const res = await axios.post('https://api.line.me/v2/bot/message/reply', data, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.CHANNEL_TOKEN}`
     }
   })
+  console.log(res)
 
-  callback(null,{
-    statusCode:200,
-    body:JSON.stringify(event)
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(event)
   })
 }
